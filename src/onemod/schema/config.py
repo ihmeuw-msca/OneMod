6656afff8900cb6451d176f47a193a8145af8fe8
Peng Zheng <zhengp@uw.edu>
2023-10-26 16:19:42 +0800

    implement the configuration classes

--- src/onemod/schema/config.py
+++ src/onemod/schema/config.py
@@ -1,131 +1,129 @@
-from typing import Optional
+from typing import Any
 
-import pandas as pd
 from pydantic import BaseModel, ConfigDict
 from pydantic.functional_validators import field_validator
 
 from modrover.globals import model_type_dict
 
+CFG_ALIASES = {
+    "mtype": "model_type",
+}
 
 
-
-
-class ParametrizedBaseModel(BaseModel):
+class CFG(BaseModel):
     """An extension of BaseModel that supports __getitem__ and is configured."""
-    model_config = ConfigDict(extra='allow', frozen=False, validate_assignment=True)
 
-    def __getitem__(self, item):
-        return getattr(self, item)
+    model_config = ConfigDict(extra="allow", frozen=False, validate_assignment=True)
 
-class RoverConfiguration(ParametrizedBaseModel):
+    @property
+    def extra_fields(self) -> set[str]:
+        return set(self.__dict__) - set(self.model_fields)
 
+    def __getitem__(self, key: str) -> Any:
+        if key in self.model_fields:
+            return getattr(self, key)
+        raise KeyError(f"{key} is not in the {self.model_fields=:}")
+
+    def model_dump(self, *args, **kwargs) -> dict[str, Any]:
+        """Overwrite the parent function to change name in the dictionary
+        accroding to CFG_ALIASES.
+        """
+        cfg = super().model_dump(*args, **kwargs)
+        keys_to_rename = [key for key in cfg.keys() if key in CFG_ALIASES]
+        for key in keys_to_rename:
+            cfg[CFG_ALIASES[key]] = cfg.pop(key)
+        return cfg
+
+
+class StageCFG(CFG):
     groupby: list[str] = []
-    model_type: str  # TODO: This clashes with pydantic naming conventions and will raise warnings
+    max_attempts: int = 1
+    max_batch: int = -1
+
+
+class RoverCFG(CFG):
+    mtype: str
     cov_fixed: list[str] = []
     cov_exploring: list[str] = []
     weights: str
     holdouts: list[str] = []
-    fit_args: dict = {}
 
-    parent_args: dict = {}
-
-    @field_validator("model_type")
+    @field_validator("mtype")
     @classmethod
-    def valid_model_type(cls, model_type: str) -> str:
-        assert model_type in model_type_dict, \
-            f"model_type must be one of {model_type_dict.keys()}"
-        return model_type
+    def valid_model_type(cls, mtype: str) -> str:
+        assert (
+            mtype in model_type_dict
+        ), f"{mtype=:} is not one of {model_type_dict.keys()}"
+        return mtype
 
-    @field_validator("fit_args")
+
+class RoverCovSelCFG(StageCFG):
+    rover: RoverCFG
+    rover_fit: dict = {}
+
+    @field_validator("rover_fit")
     @classmethod
-    def valid_fit_args(cls, fit_args: dict):
+    def valid_rover_fit(cls, rover_fit: dict):
         # TODO: Necessary or not to import and validate?
         # Could import Rover.fit and inspect the args
-        return fit_args
+        return rover_fit
 
 
-class RegmodSmoothConfiguration(ParametrizedBaseModel):
-
-    model_type: str
+class ModelCFG(CFG):
+    mtype: str
     dims: list[dict] = []
     var_groups: list[dict] = []
     weights: str
-    fit_args: dict = {}
-    inv_link: str
     coef_bounds: dict[str, list[float]] = {}
     lam: float = 0.0
 
-    parent_args: dict = {}
-
-    @field_validator("model_type")
+    @field_validator("mtype")
     @classmethod
-    def valid_model_type(cls, model_type: str) -> str:
-        assert model_type in model_type_dict, \
-            f"model_type must be one of {model_type_dict.keys()}"
-        return model_type
+    def valid_model_type(cls, mtype: str) -> str:
+        assert (
+            mtype in model_type_dict
+        ), f"{mtype=:} is not one of {model_type_dict.keys()}"
+        return mtype
 
 
-class WeaveConfiguration(ParametrizedBaseModel):
+class RegmodSmoothCFG(StageCFG):
+    model: ModelCFG
+    model_fit: dict = {}
+
+    @field_validator("model_fit")
+    @classmethod
+    def valid_rover_fit(cls, model_fit: dict):
+        # TODO: Necessary or not to import and validate?
+        # Could import Model.fit and inspect the args
+        return model_fit
+
+
+class WeaveCFG(StageCFG):
     # TODO
     pass
 
 
-class SwimrConfiguration(ParametrizedBaseModel):
+class SwimrCFG(StageCFG):
     # TODO
     pass
 
 
-class EnsembleConfiguration(ParametrizedBaseModel):
+class EnsembleCFG(StageCFG):
     # TODO
     pass
 
 
-class ParentConfiguration(ParametrizedBaseModel):
-
+class OneModCFG(CFG):
     input_path: str
     col_id: list[str]
     col_obs: str
     col_pred: str
     col_holdout: list[str]
     col_test: str
-    col_sigma: str = ''
-    max_attempts: int = 3
-    max_batch: int = -1
+    col_sigma: str = ""
 
-    rover_covsel: Optional[RoverConfiguration] = None
-    regmod_smooth: Optional[RegmodSmoothConfiguration] = None
-    weave: Optional[WeaveConfiguration] = None
-    swimr: Optional[SwimrConfiguration] = None
-    ensemble: Optional[EnsembleConfiguration] = None
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        # Pass global attributes to children
-        global_vals = {
-            "input_path": self.input_path,
-            "col_id": self.col_id,
-            "col_obs": self.col_obs,
-            "col_pred": self.col_pred,
-            "col_holdout": self.col_holdout,
-            "col_test": self.col_test,
-            "col_sigma": self.col_sigma,
-            "max_attempts": self.max_attempts,
-            "max_batch": self.max_batch,
-        }
-
-        child_models = [
-            self.rover_covsel,
-            self.regmod_smooth,
-            self.weave,
-            self.swimr,
-            self.ensemble
-        ]
-
-        for child_model in child_models:
-            if child_model:
-                # Store parent args on the child models, can be accessed if necessary
-                child_model.parent_args = global_vals
-
-    @property
-    def extra_fields(self) -> set[str]:
-        return set(self.__dict__) - set(self.model_fields)
+    rover_covsel: RoverCovSelCFG | None = None
+    regmod_smooth: RegmodSmoothCFG | None = None
+    weave: WeaveCFG | None = None
+    swimr: SwimrCFG | None = None
+    ensemble: EnsembleCFG | None = None
