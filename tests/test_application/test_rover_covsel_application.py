77f395e19b7b78f26b176bbe3feb623cda22a6b1
davidshaw-uw <dhs2018@uw.edu>
2023-12-20 16:12:00 -0800

    all tests passing

--- tests/test_application/test_rover_covsel_application.py
+++ tests/test_application/test_rover_covsel_application.py
@@ -9,4 +9,4 @@ def test_rover_covsel_application(temporary_directory, sample_input_data):
     actions = list(rover_covsel_app.action_generator())
     assert len(actions) == 7  # 3 age groups * 2 sex ids = 6 + 1 collect results
     assert actions[0].name == "rover_covsel_model"
-    assert actions[-1].name == "collect_rover_covsel_results"
+    assert actions[-1].name == "collect_results"
