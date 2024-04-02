11f98ed702556678d01809d5991b2d4cb95a0053
Peng Zheng <zhengp@uw.edu>
2024-04-02 09:38:10 +0800

    add settings to the ensemble configration class

--- src/onemod/schema/models/ensemble_config.py
+++ src/onemod/schema/models/ensemble_config.py
@@ -2,5 +2,12 @@ from onemod.schema.models.base import ParametrizedBaseModel
 
 
 class EnsembleConfiguration(ParametrizedBaseModel):
-    # TODO
-    pass
+    groupby: list[str] = []
+    max_attempts: int | None = None
+    metric: str = "rmse"
+    score: str = "neg_exp"
+    top_pct_score: float = 1.0
+    top_pct_model: float = 1.0
+
+    def inherit(self) -> None:
+        super().inherit(keys=["groupby", "max_attempts"])
