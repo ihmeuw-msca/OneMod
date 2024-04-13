cfc880390cd99106a8451faf2d2e38dd5f820a6a
Peng Zheng <zhengp@uw.edu>
2024-04-13 14:18:09 +0800

    add documentation on the schema classes

--- src/onemod/schema/stages/ensemble.py
+++ src/onemod/schema/stages/ensemble.py
@@ -2,7 +2,38 @@ from onemod.schema.base import StageConfig
 
 
 class EnsembleConfig(StageConfig):
-    """Configuration class for ensemble stage. All fields have default values."""
+    """Configuration class for ensemble stage. All fields have default values.
+
+    Parameters
+    ----------
+    metric
+        Metric to use for model evaluation. Default is "rmse".
+    score
+        Score to compute the model ensemble weights. Default is "neg_exp".
+    top_pct_score
+        Percentage of models have the top score to consider for ensemble
+        weights. Default is 1.0, which means all models are considered.
+    top_pct_model
+        Percentage of models to consider for ensemble weights. Default is 1.0,
+        which means all models are considered.
+
+    Example
+    -------
+    All of the fields have default values. And it is equivalent to the following
+    configuration for the ensemble section.
+
+    .. code-block:: yaml
+
+        ensemble:
+          groupby: []
+          max_attempts: 1
+          max_batch: -1
+          metric: rmse
+          score: neg_exp
+          top_pct_score: 1.0
+          top_pct_model: 1.0
+
+    """
 
     metric: str = "rmse"
     score: str = "neg_exp"
--- src/onemod/schema/stages/regmod_smooth.py
+++ src/onemod/schema/stages/regmod_smooth.py
@@ -2,7 +2,29 @@ from onemod.schema.base import Config, StageConfig
 
 
 class ModelInit(Config):
-    """Arguments for initialiation of a regmodsm model."""
+    """Arguments for initialiation of a regmodsm model. To create a regmodsm
+    model, we need additional configuration `mtype`, `obs`, `weights`, from the
+    global config. For more details please check
+    `regmodsm <https://github.com/ihmeuw-msca/regmodsm>`_.
+
+    Parameters
+    ----------
+    dims
+        List of dictionaries with dimension information. Default is an empty
+        list. When `age_mid` as a dimension is not provided. It will be
+        automatically created and added to the list.
+    var_groups
+        List of dictionaries with variable group information. Default is an
+        empty list. This list is considered as a suppliment list to the selected
+        covariates with dimension `age_mid` from `rover_covsel` stage. Those
+        variables will be automatically created and added to the list.
+    coef_bounds
+        Dictionary of coefficient bounds. Default is an empty dictionary.
+    lam
+        Default smoothing parameter, you can overwrite this value when define
+        variable groups in `var_groups`. Default is 0.0.
+
+    """
 
     dims: list[dict] = []
     var_groups: list[dict] = []
@@ -15,7 +37,32 @@ class RegmodSmoothConfig(StageConfig):
     """RegmodSmooth stage configuration class. Will need the `mtype` form the
     upper level to initialize a regmodsm model.
 
+    Parameters
+    ----------
+    xmodel
+        Model initialization arguments.
+    xmodel_fit
+        Model fit function arguments.
+
+    Example
+    -------
+    All of the fields have default values. And it is equivalent to the following
+    configuration for the regmod_smooth section.
+
+    .. code-block:: yaml
+
+        regmod_smooth:
+          groupby: []
+          max_attempts: 1
+          max_batch: -1
+          xmodel:
+            dims: []
+            var_groups: []
+            coef_bounds: {}
+            lam: 0.0
+          xmodel_fit: {}
+
     """
 
-    model: ModelInit
-    model_fit: dict = {}
+    xmodel: ModelInit = ModelInit()
+    xmodel_fit: dict = {}
