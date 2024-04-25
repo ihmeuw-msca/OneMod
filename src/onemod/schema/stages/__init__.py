d7de2e0c79c2d299218166633047d7bc39987bf6
Peng Zheng <zhengp@uw.edu>
2024-04-25 15:10:29 -0700

    add __all__ to exporting modules classes

--- src/onemod/schema/stages/__init__.py
+++ src/onemod/schema/stages/__init__.py
@@ -1,5 +1,11 @@
-# flake8: noqa
 from onemod.schema.stages.ensemble import EnsembleConfig
 from onemod.schema.stages.regmod_smooth import RegmodSmoothConfig
 from onemod.schema.stages.rover_covsel import RoverCovselConfig
 from onemod.schema.stages.weave import WeaveConfig
+
+__all__ = [
+    EnsembleConfig,
+    RegmodSmoothConfig,
+    RoverCovselConfig,
+    WeaveConfig,
+]
