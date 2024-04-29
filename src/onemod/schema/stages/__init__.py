7d52f7adf5f8dfab62acb6daaf177f80d35a2a58
Kelsey Maass <kmaass@uw.edu>
2024-04-29 10:04:46 -0700

    add weave classes

--- src/onemod/schema/stages/__init__.py
+++ src/onemod/schema/stages/__init__.py
@@ -1,7 +1,7 @@
 from onemod.schema.stages.ensemble import EnsembleConfig
 from onemod.schema.stages.regmod_smooth import RegmodSmoothConfig
 from onemod.schema.stages.rover_covsel import RoverCovselConfig
-from onemod.schema.stages.weave import WeaveConfig
+from onemod.schema.stages.weave import WeaveConfig, WeaveDimension, WeaveModel
 
 __all__ = [
     EnsembleConfig,
