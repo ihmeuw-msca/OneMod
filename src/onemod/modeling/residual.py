45f355eb97fd064afbf404ba2e77bfc69e7cc5ed
zhengp0 <zhengp@uw.edu>
2024-05-06 16:30:39 -0700

    add the predict function to residual calculator

--- src/onemod/modeling/residual.py
+++ src/onemod/modeling/residual.py
@@ -6,6 +6,7 @@ class ResidualCalculator:
         if not hasattr(self, f"get_residual_{model_type}"):
             raise AttributeError(f"'{model_type}' is not a valid model type")
         self.model_type = model_type
+        self.predict = getattr(self, f"predict_{model_type}")
 
     @staticmethod
     def get_residual_binomial(
@@ -21,6 +22,12 @@ class ResidualCalculator:
         return result
 
     @staticmethod
+    def predict_binomial(
+        data: pd.DataFrame, pred: str, residual: str = "residual"
+    ) -> pd.Series:
+        return data.eval(f"{residual} * {pred} * (1 - {pred})")
+
+    @staticmethod
     def get_residual_poisson(
         data: pd.DataFrame, pred: str, obs: str, weights: str
     ) -> pd.DataFrame:
@@ -30,6 +37,12 @@ class ResidualCalculator:
         return result
 
     @staticmethod
+    def predict_poisson(
+        data: pd.DataFrame, pred: str, residual: str = "residual"
+    ) -> pd.Series:
+        return data.eval(f"({residual} + 1) * {pred}")
+
+    @staticmethod
     def get_residual_gaussian(
         data: pd.DataFrame, pred: str, obs: str, weights: str
     ) -> pd.DataFrame:
@@ -37,6 +50,12 @@ class ResidualCalculator:
         result["residual"] = data.eval(f"{obs} - {pred}")
         result["residual_se"] = data.eval(f"1 / sqrt({weights})")
         return result
+
+    @staticmethod
+    def predict_gaussian(
+        data: pd.DataFrame, pred: str, residual: str = "residual"
+    ) -> pd.Series:
+        return data.eval(f"{pred} + {residual}")
 
     def __call__(self, *args, **kwargs) -> pd.DataFrame:
         return getattr(self, f"get_residual_{self.model_type}")(*args, **kwargs)
