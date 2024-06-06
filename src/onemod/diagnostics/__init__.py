3309bbe08393ae66509ba50e7d45ab8f73e83643
Kelsey Maass <kmaass@uw.edu>
2024-06-06 07:56:38 -0700

    add stage plotting functions to init

--- src/onemod/diagnostics/__init__.py
+++ src/onemod/diagnostics/__init__.py
@@ -1,5 +1,7 @@
-from onemod.diagnostics.plots import plot_results
-
-__all__ = [
+from onemod.diagnostics.plots import (
     plot_results,
-]
+    plot_rover_covsel_results,
+    plot_spxmod_results,
+)
+
+__all__ = [plot_results, plot_rover_covsel_results, plot_spxmod_results]
