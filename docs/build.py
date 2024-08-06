af767a22b972b7af017c8854e0f4d33b8266511b
gphipps <Guinop4>
2024-08-06 13:40:10 -0700

    Testing locally

--- docs/build.py
+++ docs/build.py
@@ -9,14 +9,13 @@ run = functools.partial(subprocess.run, shell=True)
 def build_doc(version: str) -> None:
     print(f"Build _{version}_")
     run(f"git checkout v{version}")
-    run("git checkout publish-docs -- docs/conf.py")
-    run("git checkout publish-docs -- docs/versions.toml")
+    run("git checkout publish-docs -- conf.py")
+    run("git checkout publish-docs -- versions.toml")
 
-    #run("make html")
-    run("sphinx-build -M html docs _build")
-    run("ls _build")
-    run(f"mv _build/html pages/{version}")
-    run("rm -rf _build")
+    run("sphinx-build -M html ../docs ../_build")
+    run("ls ../_build")
+    run(f"mv ../_build/html ../pages/{version}")
+    run("rm -rf ../_build")
     run("git checkout publish-docs")
 
 
@@ -27,17 +26,17 @@ def build_init_page(version: str) -> None:
 
 
 if __name__ == "__main__":
+    # Executes in the docs directory, hence the constant use of ..
     # create pages folder
     print("Python main")
-    run("rm -rf pages")
-    run("mkdir pages")
+    run("rm -rf ../pages")
+    run("mkdir ../pages")
 
     # get versions
-    with open("docs/meta.toml", "rb") as f:
+    with open("meta.toml", "rb") as f:
         versions = tomllib.load(f)["versions"]
-    print(f"versions A _{versions}_")
     versions.sort(reverse=True, key=lambda v: tuple(map(int, v.split("."))))
-    print(f"versions B _{versions}_")
+    print(f"versions _{versions}_")
 
     # build documentations for different versions
     for version in versions:
