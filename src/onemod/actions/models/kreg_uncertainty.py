26d9c01fc38578ef5eff1d4e94c7e735767c9fec
Kelsey Maass <kmaass@uw.edu>
2024-08-15 13:14:23 -0700

    update column names

--- src/onemod/actions/models/kreg_uncertainty.py
+++ src/onemod/actions/models/kreg_uncertainty.py
@@ -64,21 +64,15 @@ def kreg_uncertainty(directory: str, submodel_id: str) -> None:
         ignore_index=True,
     )
 
-    # Zero test data and rescale age
-    # TODO: generalize offset for different model types
+    # Zero test data
     index = data.eval(f"{config.test} == 1")
     data.loc[index, config.obs] = 0.0
     data.loc[index, config.weights] = 0.0
-    age_scale = config.kreg.kreg_model.age_scale
-    data["transformed_age"] = data.eval(
-        "log(exp(@age_scale * age_mid) - 1) / @age_scale"
-    )
 
     # Load kernel regression model
     model = dataif.load_kreg(f"submodels/{submodel_id}/model.pkl")
     model.likelihood.attach(data)
-    op_hess = model.hessian(jnp.asarray(data["kreg_y"]))
-    data["offset"] = model.likelihood.data["offset"]
+    op_hess = model.hessian(jnp.asarray(data["kreg"]))
 
     def op_root_pc(x):
         return model.kernel.op_root_k @ x
@@ -90,65 +84,70 @@ def kreg_uncertainty(directory: str, submodel_id: str) -> None:
     sampler = jax.jit(get_PC_inv_rootH(op_pced_hess, op_root_pc, lanczos_order))
     moment = jnp.zeros(len(data))
     if save_draws:
-        error_draw_cols = [f"kreg_error_draw_{i}" for i in range(num_samples)]
+        error_draw_cols = [f"error_draw_{i}" for i in range(num_samples)]
     for i in tqdm(range(num_samples)):
         probe = jax.random.normal(jax.random.PRNGKey(i), (len(data),))
         sample = sampler(probe)
         moment += sample**2
         if save_draws:
-            data[f"kreg_error_draw_{i}"] = sample
+            data[f"error_draw_{i}"] = sample
 
-    data["kreg_y_sd"] = jnp.sqrt(moment / num_samples)
-    data["kreg_linear"] = data.eval("kreg_y + offset")
+    data["kreg_sd"] = jnp.sqrt(moment / num_samples)
+    data["kreg_with_offset"] = data.eval("kreg + offset")
 
-    data["kreg_lwr"] = expit(data.eval("kreg_linear - 1.96 * kreg_y_sd"))
-    data["kreg_upr"] = expit(data.eval("kreg_linear + 1.96 * kreg_y_sd"))
+    data["kreg_lwr"] = expit(data.eval("kreg_with_offset - 1.96 * kreg_sd"))
+    data["kreg_upr"] = expit(data.eval("kreg_with_offset + 1.96 * kreg_sd"))
 
     # Calibrate uncertainty by region
     # TODO: hard coded by region
-    data["cali_kreg_y_sd"] = data["kreg_y_sd"]
+    data[f"{config.pred}_sd"] = data["kreg_sd"]
     data_group = data.groupby("region_id")
     if save_draws:
-        cali_draw_cols = [f"cali_kreg_draw_{i}" for i in range(num_samples)]
+        pred_draw_cols = [f"{config.pred}_draw_{i}" for i in range(num_samples)]
 
     for key, data_sub in data_group:
-        cali_kreg_y_sd = calibrate_pred_sd(
+        pred_sd = calibrate_pred_sd(
             data_sub.query(f"{config.test} == 0"),
             config.mtype,
-            "kreg_linear",
-            "kreg_y_sd",
+            "kreg_with_offset",
+            "kreg_sd",
             config.obs,
             config.weights,
         )
         alpha = (
-            cali_kreg_y_sd / data_sub.query(f"{config.test} == 0")["kreg_y_sd"]
+            pred_sd / data_sub.query(f"{config.test} == 0")["kreg_sd"]
         ).mean()
         index = data_group.groups[key]
-        data.loc[index, "cali_kreg_y_sd"] = alpha * data.loc[index, "kreg_y_sd"]
+        data.loc[index, f"{config.pred}_sd"] = (
+            alpha * data.loc[index, "kreg_sd"]
+        )
         if save_draws:
-            data.loc[index, cali_draw_cols] = expit(
-                data.loc[index, ["kreg_linear"]].values
+            data.loc[index, pred_draw_cols] = expit(
+                data.loc[index, ["kreg_with_offset"]].values
                 + alpha * data.loc[index, error_draw_cols].values
             )
 
-    data["cali_kreg_lwr"] = expit(
-        data.eval("kreg_linear - 1.96 * cali_kreg_y_sd")
+    data[f"{config.pred}_lwr"] = expit(
+        data.eval(f"kreg_with_offset - 1.96 * {config.pred}_sd")
     )
-    data["cali_kreg_upr"] = expit(
-        data.eval("kreg_linear + 1.96 * cali_kreg_y_sd")
+    data[f"{config.pred}_upr"] = expit(
+        data.eval(f"kreg_with_offset + 1.96 * {config.pred}_sd")
     )
 
     # Save results
     result_columns = [
         "offset",
-        config.pred,
+        "kreg",
+        "kreg_sd",
         "kreg_lwr",
         "kreg_upr",
-        "cali_kreg_lwr",
-        "cali_kreg_upr",
+        config.pred,
+        f"{config.pred}_sd",
+        f"{config.pred}_lwr",
+        f"{config.pred}_upr",
     ]
     if save_draws:
-        result_columns += cali_draw_cols
+        result_columns += pred_draw_cols
     dataif.dump_kreg(
         data[config.ids + result_columns],
         f"submodels/{submodel_id}/predictions.parquet",
