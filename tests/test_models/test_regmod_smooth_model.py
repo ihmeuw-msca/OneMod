f88695a669c96ce7d47c1ba0ba9f44a67b248fae
gphipps <Guinop4>
2024-08-29 09:37:21 -0700

    Test setup

--- tests/test_models/test_regmod_smooth_model.py
+++ tests/test_models/test_regmod_smooth_model.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 import yaml
 
-# from onemod.data.initialize_results import initialize_results
+from onemod.actions.data.initialize_results import _initialize_rover_covsel_results, _initialize_spxmod_results
 from onemod.actions.models.spxmod_model import spxmod_model
 
 
 def test_spxmod(temporary_directory):
     # Mock the rover output - selected_covs.yaml
-    # initialize_results(directory=temporary_directory,
-    #                    stages=["rover_covsel", "spxmod"])
+    _initialize_rover_covsel_results(dataif=temporary_directory)
+    _initialize_spxmod_results(dataif=temporary_directory)
     selected_covs = ["cov1", "cov2"]
     yaml_path = (
         temporary_directory / "results" / "rover_covsel" / "selected_covs.yaml"
