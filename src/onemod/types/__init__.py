4978caeb770c296c5b1770abcdbd71fcd22f04f0
Wes Warriner <fridgehistory@gmail.com>
2024-10-01 08:23:54 -0700

    feat: export all custom types

---
+++ src/onemod/types/__init__.py
@@ -0,0 +1,13 @@
+from onemod.types.array import Array
+from onemod.types.data import Data
+from onemod.types.filepath import FilePath
+from onemod.types.float import Float
+from onemod.types.integer import Integer
+
+__all__ = [
+    "Array",
+    "Data",
+    "FilePath",
+    "Float",
+    "Integer",
+]
