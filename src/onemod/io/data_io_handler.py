7d4c334a3c9c69b805a96fe3fc349850dfec632e
Wes Warriner <fridgehistory@gmail.com>
2024-10-06 21:09:58 -0700

    feat: add DataIOHandler; update Data columns validation, add Data serialization

---
+++ src/onemod/io/data_io_handler.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from polars import DataFrame, read_parquet
+
+
+class DataIOHandler:
+    """
+    Handler for reading and writing data in Parquet format.
+    
+    Notes
+    _____
+    * Future: Add support for other file formats.
+    """
+    
+    @staticmethod
+    def read_data(path: Path) -> DataFrame:
+        """Read data from a Parquet file and return it as a Polars DataFrame."""
+        if path.suffix != '.parquet':
+            raise ValueError(f"Unsupported file format: {path.suffix}.")
+        
+        try:
+            df = read_parquet(path)
+        except Exception as e:
+            raise IOError(f"Failed to read data from {path}: {e}")
+        
+        return df
+
+    @staticmethod
+    def write_data(df: DataFrame, path: Path) -> None:
+        """Write a Polars DataFrame to a Parquet file."""
+        try:
+            df.write_parquet(path)
+        except Exception as e:
+            raise IOError(f"Failed to write data to {path}: {e}")
--- src/onemod/types/data.py
+++ src/onemod/types/data.py
@@ -1,26 +1,129 @@
-from typing import Any, Dict, Union
+from pathlib import Path
+from typing import Any, Dict, Optional, Union
 
-from pandas import DataFrame
+from polars import Boolean, DataFrame, Int64, Float64, String
 from pydantic import BaseModel
+
+from onemod.constraints import Constraint
+from onemod.io import DataIOHandler
+from onemod.serializers import deserialize, serialize
+from onemod.types import ColumnSpec, FilePath
+from onemod.validation import collector as validation_collector
 
 
 class Data(BaseModel):
-    columns: Dict[str, Any]
+    stage: str
+    path: FilePath
+    format: str = "parquet"
+    shape: Optional[tuple[int, int]] = None
+    columns: Optional[Dict[str, ColumnSpec]] = None
+    
+    TYPE_MAPPING = {
+        bool: Boolean,
+        int: Int64,
+        float: Float64,
+        str: String,
+    }
+    
+    @classmethod
+    def from_dict(cls, data_dict: dict) -> 'Data':
+        """Reconstruct a Data object from a dictionary."""
+        return cls(
+            stage=data_dict["stage"],
+            path=Path(data_dict["path"]),
+            format=data_dict.get("format", "parquet"),
+            shape=tuple(data_dict["shape"]) if data_dict.get("shape") else None,
+            columns={
+                col_name: {
+                    "type": col_spec.get("type", Any),
+                    "constraints": [
+                        Constraint.from_dict(constraint)
+                        for constraint in col_spec.get("constraints", [])
+                    ]
+                }
+                for col_name, col_spec in data_dict.get("columns", {}).items()
+            }
+        )
+
+    def to_dict(self) -> dict:
+        """Convert the Data object to a dictionary."""
+        return {
+            "stage": self.stage,
+            "path": str(self.path),
+            "format": self.format,
+            "shape": self.shape,
+            "columns": {
+                col_name: {
+                    "type": col_spec["type"].__name__,
+                    "constraints": [constraint.to_dict() for constraint in col_spec.get("constraints", [])]
+                }
+                for col_name, col_spec in (self.columns or {}).items()
+            }
+        }
+    
+    @classmethod
+    def from_config(cls, config_path: Union[str, Path]) -> 'Data':
+        """Load a Data configuration from a YAML or JSON file."""
+        config = deserialize(config_path)
+        return cls(**config)
+
+    def to_config(self, config_path: Union[str, Path]) -> None:
+        """Save the current Data configuration to a YAML or JSON file."""
+        serialize(self, config_path)
 
     @classmethod
-    def with_columns(cls, columns: Dict[str, Union[type, BaseModel]]) -> 'Data':
-        """Creates a Data object with specified column types."""
-        return cls(columns=columns)
+    def use_validation(
+        cls,
+        columns: Optional[Dict[str, ColumnSpec]] = None,
+        shape: Optional[tuple[int, int]] = None
+    ) -> 'Data':
+        """Specify validation for shape, column types and column constraints."""
+        return cls(columns=columns, shape=shape)
 
+    @classmethod
+    def with_columns(cls, columns: Dict[str, ColumnSpec]) -> 'Data':
+        """Specify expected columns and their types."""
+        return cls(columns=columns)
+    
+    @classmethod
+    def with_shape(cls, rows: int, cols: int) -> 'Data':
+        """Specify the expected shape of the DataFrame (rows, columns)."""
+        return cls(shape=(rows, cols))
+    
+    def validate_metadata(self) -> None:
+        """One-time validation for instance metadata."""
+        if not self.path:
+            validation_collector.add_error(self.stage, "Data validation", "File path is required.")
+        
+        if self.shape:
+            if not isinstance(self.shape, tuple) or len(self.shape) != 2:
+                validation_collector.add_error(self.stage, "Data validation", "Shape must be a tuple of (rows, columns).")
+
+    def validate_data(self) -> None:
+        """Validate the columns and shape of the data."""
+        data = DataIOHandler.read_data(self.path)
+        
+        if self.shape and data.shape != self.shape:
+            validation_collector.add_error(self.stage, "Data validation", f"Expected DataFrame shape {self.shape}, got {data.shape}.")
+        
+        if self.columns:
+            self.validate_columns(data)
+    
     def validate_columns(self, data: DataFrame) -> None:
-        """Validate that each row in the DataFrame matches the specified type for that column."""
-        for index, row in data.iterrows():  # TODO: computational overhead worth the benefit of pre-run validation?
-            for col_name, col_type in self.columns.items():  # We could just do this loop alone to check dtypes, but we would lose the constraints checking functionality
-                if col_name in row:
-                    value = row[col_name]
-                    if not isinstance(value, col_type):
-                        raise ValueError(
-                            f"Column '{col_name}' in row {index} expected to be of type {col_type}, but got {type(value)}"
-                        )
-                else:
-                    raise ValueError(f"Missing required column: '{col_name}' in row {index}")
+        """Validate columns based on specified types and constraints."""
+        for col_name, col_spec in self.columns.items():
+            if col_name not in data.columns:
+                validation_collector.add_error(self.stage, "Data validation", f"Column '{col_name}' is missing from the data.")
+            
+            expected_type = col_spec.get('type')
+            constraints = col_spec.get('constraints', [])
+            
+            if expected_type:
+                polars_type = self.TYPE_MAPPING.get(expected_type)  # TODO: better ways to handle this?
+                if not polars_type:
+                    validation_collector.add_error(self.stage, "Data validation", f"Unsupported type {expected_type}.")
+                if data[col_name].dtype != polars_type:
+                    validation_collector.add_error(self.stage, "Data validation", f"Column '{col_name}' must be of type {expected_type.__name__}.")
+            
+            for constraint in constraints:
+                constraint(data[col_name])
