1bd5a8b2389a01eb80bad8292f89741772666e36
Kelsey Maass <kmaass@uw.edu>
2024-10-08 07:42:40 -0700

    update test_serialize function

--- tests/test_output.py
+++ tests/test_output.py
@@ -16,53 +16,47 @@ import pytest
 from onemod.io import Data, Output
 
 
-TEST_OUTPUT = Output(
-    stage="test_stage",
-    items={
-        "predictions": {
-            "stage": "test_stage",
-            "path": "/path/to/predictions.parquet",
-        }
-    },
-)
+ITEMS = {
+    "predictions": {"stage": "stage", "path": "/path/to/predictions.parquet"}
+}
+OUTPUT = Output(stage="stage", items=ITEMS)
 
 
 def test_serialize():
-    assert TEST_OUTPUT.model_dump() == TEST_OUTPUT.items
+    assert OUTPUT.model_dump() == ITEMS
 
 
 def test_get():
-    assert TEST_OUTPUT.get("predictions") == Data(
-        stage="test_stage", path=Path("/path/to/predictions.parquet")
+    assert OUTPUT.get("predictions") == Data(
+        stage="stage", path=Path("/path/to/predictions.parquet")
     )
-    assert TEST_OUTPUT.get("dummy") is None
-    assert TEST_OUTPUT.get("dummy", "default") == "default"
+    assert OUTPUT.get("dummy") is None
+    assert OUTPUT.get("dummy", "default") == "default"
 
 
 def test_getitem():
-    assert TEST_OUTPUT["predictions"] == Data(
-        stage="test_stage", path=Path("/path/to/predictions.parquet")
+    assert OUTPUT["predictions"] == Data(
+        stage="stage", path=Path("/path/to/predictions.parquet")
     )
     with pytest.raises(KeyError) as error:
-        TEST_OUTPUT["dummy"]
+        OUTPUT["dummy"]
     print(error.value)
     print(str(error.value))
     assert (
-        str(error.value).strip('"')
-        == "test_stage does not contain output 'dummy'"
+        str(error.value).strip('"') == "stage does not contain output 'dummy'"
     )
 
 
 def test_contains():
-    assert "predictions" in TEST_OUTPUT
-    assert "dummy" not in TEST_OUTPUT
+    assert "predictions" in OUTPUT
+    assert "dummy" not in OUTPUT
 
 
 def test_frozen():
     with pytest.raises(ValidationError):
-        TEST_OUTPUT.items = {}
+        OUTPUT.items = {}
 
 
 def test_no_setitem():
     with pytest.raises(TypeError):
-        TEST_OUTPUT["key"] = "value"
+        OUTPUT["item_name"] = "item_value"
