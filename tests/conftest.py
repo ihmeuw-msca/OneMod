d7801e59131818fd4b20b42a7add8fbcedb6cb18
Wes Warriner <fridgehistory@gmail.com>
2024-10-09 09:59:17 -0700

    rm comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,4 +20,4 @@ def test_assets_dir():
 def validation_collector() -> Generator[ValidationErrorCollector, None, None]:
     """Fixture that manages the validation context for tests."""
     with validation_context() as collector:
-        yield collector  # Provide the collector to the test
+        yield collector
