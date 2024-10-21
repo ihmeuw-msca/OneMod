a65b7b13e60f65f26bfc8b761d374a37b757a670
Kelsey Maass <kmaass@uw.edu>
2024-10-20 19:29:28 -0700

    check filetype in filter_data, allow subset of subset with id_names in get_subset

--- src/onemod/utils/subsets.py
+++ src/onemod/utils/subsets.py
@@ -19,28 +19,43 @@ def create_subsets(groupby: set[str], data: Path | str) -> pd.DataFrame | None:
 
 
 def get_subset(
-    data: Path | str, subsets: Path | str, subset_id: int
+    data: Path | str,
+    subsets: Path | str,
+    subset_id: int,
+    id_names: list[str] | None = None,
 ) -> pd.DataFrame:
     """Get data subset by subset_id."""
-    subset = pd.read_csv(subsets).query("subset_id == @subset_id")
-    id_subsets = {
-        key: [value.item()]
-        for key, value in subset.items()
-        if key != "subset_id"
-    }
+    id_subsets = get_id_subsets(subsets, subset_id)
+    if id_names is not None:
+        id_subsets = {
+            key: value for key, value in id_subsets.items() if key in id_names
+        }
     return filter_data(data, id_subsets)
 
 
-def filter_data(
-    data: Path | str, id_subsets: dict[str, set[int]]
-) -> pd.DataFrame:
-    """Filter data by id subsets."""
+def get_id_subsets(subsets: Path | str, subset_id: int) -> dict:
+    """Get ID names and values that define a data subset."""
     return (
-        pd.read_parquet(data)
-        .query(
-            " & ".join(
-                [f"{key}.isin({value})" for key, value in id_subsets.items()]
-            )
-        )
-        .reset_index(drop=True)
+        pd.read_csv(subsets)
+        .query("subset_id == @subset_id")
+        .drop(columns=["subset_id"])
+        .iloc[0]
+        .to_dict()
     )
+
+
+def filter_data(
+    data_path: Path | str, id_subsets: dict[str, set[int]]
+) -> pd.DataFrame:
+    """Filter data by ID subsets."""
+    if (suffix := Path(data_path).suffix) == ".csv":
+        data = pd.read_csv(data_path)
+    elif suffix == ".parquet":
+        data = pd.read_parquet(data_path)
+    else:
+        raise ValueError(f"Unsupported file type: {suffix}")
+    return data.query(
+        " & ".join(
+            [f"{key}.isin({value})" for key, value in id_subsets.items()]
+        )
+    ).reset_index(drop=True)
