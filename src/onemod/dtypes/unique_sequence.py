17a6dd8a5ce41098c3ea27c5e6cf1e401b8364d1
Kelsey Maass <kmaass@uw.edu>
2025-01-16 13:15:24 -0800

    simplify unique_sequence

--- src/onemod/dtypes/unique_sequence.py
+++ src/onemod/dtypes/unique_sequence.py
@@ -1,48 +1,33 @@
-"""Helper functions for unique lists and tuples.
+"""Helper functions for unique lists and tuples."""
 
-Notes
------
-There are many `Pipeline` and `Stage` fields that we expect to contain
-unique items (e.g., `groupby`, `crossby`, `stages`). We originally
-implemented these as sets, but this caused some issues. For example, the
-fact that the `groupby` set didn't preserve order caused `subset`
-ordering to be unpredictable. We also often had to cast the `groupby`
-set as a list before calling pandas functions. The functions and type
-annotations here allow us to ensure unique items while preserving order.
-Because they are only validators and not actual types, they don't
-guarantee uniqueness when fields are modified by the user.
-
-"""
-
-from typing import Hashable, List, Tuple, TypeVar
+from typing import Annotated, Hashable, TypeVar
 
 from pydantic import AfterValidator
-from typing_extensions import Annotated
 
 T = TypeVar("T", bound=Hashable)
 
 
-def unique_list(items: List[T]) -> List[T]:
+def unique_list(items: list[T]) -> list[T]:
     """Ensure all items in list are unique while preserving order."""
     return list(dict.fromkeys(items))
 
 
-def update_unique_list(list1: List[T], list2: List[T]) -> List[T]:
+def update_unique_list(list1: list[T], list2: list[T]) -> list[T]:
     """Combine two lists, remove duplicates, and preserve order."""
     return list(dict.fromkeys(list1 + list2))
 
 
-def unique_tuple(items: Tuple[T, ...]) -> Tuple[T, ...]:
+def unique_tuple(items: tuple[T, ...]) -> tuple[T, ...]:
     """Ensure all items in tuple are unique while preserving order."""
     return tuple(dict.fromkeys(items))
 
 
 def update_unique_tuple(
-    tuple1: Tuple[T, ...], tuple2: Tuple[T, ...]
-) -> Tuple[T, ...]:
+    tuple1: tuple[T, ...], tuple2: tuple[T, ...]
+) -> tuple[T, ...]:
     """Combine two tuples, remove duplicates, and preserve order."""
     return tuple(dict.fromkeys(tuple1 + tuple2))
 
 
-UniqueList = Annotated[List[T], AfterValidator(unique_list)]
-UniqueTuple = Annotated[Tuple[T, ...], AfterValidator(unique_tuple)]
+UniqueList = Annotated[list[T], AfterValidator(unique_list)]
+UniqueTuple = Annotated[tuple[T, ...], AfterValidator(unique_tuple)]
