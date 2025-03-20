3c219c85000108f91bde42bcf3b5a24a27e5a152
Leo Zoeckler <lzoeckle@uw.edu>
2025-03-20 11:04:28 -0700

    removing jobmon backend from init

--- src/onemod/__init__.py
+++ src/onemod/__init__.py
@@ -1,11 +1,9 @@
-from onemod.backend.jobmon_backend import add_tasks_to_workflow
 from onemod.config import Config, StageConfig
 from onemod.main import load_pipeline, load_stage
 from onemod.pipeline import Pipeline
 from onemod.stage import Stage
 
 __all__ = [
-    "add_tasks_to_workflow",
     "Config",
     "Pipeline",
     "Stage",
