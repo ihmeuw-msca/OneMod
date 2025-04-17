650922cf98f8053608c2e26229170688f1e4f96a
Leo Zoeckler <lzoeckle@uw.edu>
2025-04-17 15:44:15 -0700

    removin template check

--- src/onemod/backend/jobmon_backend.py
+++ src/onemod/backend/jobmon_backend.py
@@ -817,21 +817,6 @@ def get_task_template(
     # template name across all models in a given workflow.
     node_args = ["config"] + submodel_args
 
-    # If the task template already exists in the tool, check that it's
-    # identical to the new task template.
-    if template_name in tool.active_task_templates.keys():
-        existing_template = tool.active_task_templates[template_name]
-        existing_template.load_task_template_versions()
-        if len(existing_template.task_template_versions) != 1:
-            raise RuntimeError("TODO: Too many task template versions")
-        existing_template_version = existing_template.task_template_versions[0]
-        if command_template != existing_template_version.command_template:
-            raise RuntimeError("TODO: command_template")
-        if set(task_args) != existing_template_version.task_args:
-            raise RuntimeError("TODO: task_args")
-        if set(node_args) != existing_template_version.node_args:
-            raise RuntimeError("TODO: node_args")
-
     task_template = tool.get_task_template(
         template_name=template_name,
         command_template=command_template,
